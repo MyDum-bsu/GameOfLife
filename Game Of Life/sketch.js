f1e6dc24bb418fb9c7fdfd09e26eba04e27e97e4
MyDum-bsu <mydum.bsu@gmail.com>
2022-07-09 01:15:10 +0300

    You can add live cells by clicking on the left mouse button, delete live cells by clicking on the right mouse button. In this case, the blank field with the added cells will not be filled further. Fixed bugs with clicking on speed and resolution changes.

--- Game Of Life/sketch.js
+++ Game Of Life/sketch.js
@@ -1,7 +1,10 @@
 function make2DArray(cols, rows) {
   let arr = new Array(cols);
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = new Array(rows);
+  for (let i = 0; i < cols; i++) {
+    arr[i] = new Array(rows)
+    for (let j = 0; j < rows; j++) {
+      arr[i][j] = 0;
+    }
   }
   return arr;
 }
@@ -11,29 +14,24 @@ function make2DArray(cols, rows) {
   var cols;
   var rows;
   var resolution;
-  var resolutionChanged;
-  var speedChanged;
   var mode;
-  let slider;
+  let Rslider;
+  let Sslider;
+  var randomMode;
 
 function setup() {
   mode = 0;
-  resolutionChanged = 0;
-  speedChanged = 0;
-  //noCursor();
-  //frameRate(); 
-
-  canvas = createCanvas(1920, 973);
+  randomMode = 1;
+  canvas = createCanvas(1920, 951);
   canvas.parent('canvas');
 
-  Rslider = createSlider(2, 150, 10, 1);
+  Rslider = createSlider(2, 200, 5, 1);
   Sslider = createSlider(1, 60, 60, 1);
   resolution = Rslider.value();
   frameRate(Sslider.value());
   cols = floor(width / resolution);
   rows = floor(height / resolution);
   grid = make2DArray(cols, rows);
-  fillGrid();
 }
 
 function keyPressed() {
@@ -46,56 +44,21 @@ function keyPressed() {
 }
 
 function draw() {
-  //clear();
   
-  if (resolutionChanged || speedChanged) {
-    resolution = Rslider.value();
-    frameRate(Sslider.value());
-    cols = floor(width / resolution);
-    rows = floor(height / resolution);
-    grid = make2DArray(cols, rows);
-    for (let i = 0; i < cols; i++) {
-      for (let j = 0; j < rows; j++) {
-        grid[i][j] = floor(random(2));
-      }
-    }
-    resolutionChanged = 0;
-    speedChanged = 0;
-  }
+  ChangeResolution();
+
+  ChangeSpeed();
+
+  AddRemoveOnMouseClick(grid);
 
   if (mode == 1) {  
     background(30);
-      for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-          let x = i * resolution;
-          let y = j * resolution;
-          if (grid[i][j] == 1) {
-            fill(90);
-            stroke(200);
-            rect(x, y, resolution - 1, resolution - 1);
-          }
-        }
-      }  
-  
-    let next = make2DArray(cols, rows);
-  
-    for (let i = 0; i < cols; i++) {
-      for (let j = 0; j < rows; j++) {
-        let state = grid[i][j];
-        let sum = 0;
-        let neighbors = countNeighbors(grid, i, j);
-  
-        if (state == 0 && neighbors == 3) {
-          next[i][j] = 1;
-        } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
-          next[i][j] = 0;
-        } else {
-          next[i][j] = state;
-        }
-      }
+    if (randomMode == 1) {
+      fillGrid(grid, cols, rows);
+      randomMode = 0;
     }
-  
-    grid = next;
+    drawGrid(grid);   
+    grid = nextGeneration(grid);
   }
 }
 
@@ -112,7 +75,7 @@ function countNeighbors(grid, x, y) {
   return sum;
 }
 
- function fillGrid() {
+ function fillGrid(grid, cols, rows) {
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       grid[i][j] = floor(random(2));
@@ -120,7 +83,94 @@ function countNeighbors(grid, x, y) {
   }
  }
 
-function touchEnded() {
-  resolutionChanged = 1;
-  speedChanged = 1;
+ function ChangeResolution() {
+  if (resolution != Rslider.value()) {
+    background(30);
+    randomMode = 1;
+    resolution = Rslider.value();
+    cols = floor(width / resolution);
+    rows = floor(height / resolution);
+    grid = make2DArray(cols, rows);
+  }
+ }
+
+ function ChangeSpeed() {
+  if (frameRate() != Sslider.value()) {
+    frameRate(Sslider.value());
+  }
+ }
+
+ function ValidateMousePosition(x, y) {
+   return x >= 0 && y >= 0 && x < cols && y < rows;
+ }
+ 
+ function fillCell(i, j) {
+  let x = i * resolution;
+  let y = j * resolution;
+  fill(90);
+  stroke(200);
+  rect(x, y, resolution - 1, resolution - 1);
+ }
+
+ function removeCell(i, j) {
+  let x = i * resolution;
+  let y = j * resolution;
+  fill(30);
+  stroke(30);
+  rect(x, y, resolution, resolution);
+ }
+
+ function nextGeneration(grid) {
+  let next = make2DArray(cols, rows);
+  
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+      let sum = 0;
+      let neighbors = countNeighbors(grid, i, j);
+
+      if (state == 0 && neighbors == 3) {
+        next[i][j] = 1;
+      } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+        next[i][j] = 0;
+      } else {
+        next[i][j] = state;
+      }
+    }
+  }
+  return next;
+ }
+
+ function drawGrid(grid) {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] == 1) {
+        fillCell(i, j);
+      }
+    }
+  } 
+ }
+
+function AddRemoveOnMouseClick(grid) {
+  if (mouseIsPressed === true) {
+    if (mouseButton === LEFT) {
+      var x = floor(mouseX / resolution);
+      var y = floor(mouseY / resolution);
+      if (ValidateMousePosition(x, y) == 1) {
+        grid[x][y] = 1;
+        fillCell(x, y);
+        randomMode = 0;
+      }
+    }
+    if (mouseButton === RIGHT) {
+      var x = floor(mouseX / resolution);
+      var y = floor(mouseY / resolution);
+      if (ValidateMousePosition(x, y) == 1) {
+        grid[x][y] = 0;
+        removeCell(x, y);
+        }
+    }
+  }
 }
