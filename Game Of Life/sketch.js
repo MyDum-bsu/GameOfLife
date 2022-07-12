9f98a057e670a3a3a653e97b15986f88a4ef672a
MyDum-bsu <mydum.bsu@gmail.com>
2022-07-12 22:59:12 +0300

    No global changing, just plans. The TODO in sketch.js

--- Game Of Life/sketch.js
+++ Game Of Life/sketch.js
@@ -1,20 +1,35 @@
 
 let canvas = null;
-  
+let BJ_image;
+let life_image;
+
+function preload() {
+  BJ_image = loadImage('images/The_Belousov-Zhabotinsky_Reaction.gif');
+  life_image = loadImage('images/life.gif');
+}
 function setup() {
   
   canvas = createCanvas(1920, 892);
   canvas.parent('canvas');
+   Initialization();
+   CreateSettings();
+   CloseSettings();
 
-   //Initialization();
-   //CreateSettings();
-   //CloseSettings();
-
-  BJ_Initialization();
+  //BJ_Initialization();
+  //fill(255, 255, 255);
+  //  textSize(15);
+  //  textFont('Courier');
+  //textAlign(CENTER);
+  //text('Choose the game:', width / 2, height / 2);
+ 
 }
 
-function draw() {
 
+function draw() {
+ // clear();
+  //image(BJ_image, 100, height / 2 + 20);
+  //image(life_image, 700, height / 2 + 20);
+   //BJReaction();
   //GameLife();
-  BJReaction();
+ 
 }
